baf2225b93a930d430fde5034d74530aa6682e59
MatteoNocera <matteo_nocera@tiscali.it>
2023-09-21 15:12:15 +0200

    initial commit
    create files and modify App.vue to insert text hello vite

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+/* import HelloWorld from './components/HelloWorld.vue' */
+export default{
+    data() {
+        return {
+            message: 'hello vite',
+            
+          }
+      }
+  }
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style>
+* {
+  background-color: darkgray;
+}
+</style>
